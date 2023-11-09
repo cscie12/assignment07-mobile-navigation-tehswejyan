a24c3f11b34877d8719e63dbdcd49d0033ae315e
emerald-01 <teh.sjchem@gmail.com>
2023-11-09 09:34:07 +0800

    9 Nov 2023

---
+++ scripts/navigation.js
@@ -0,0 +1,18 @@
+//github repo link: https://github.com/cscie12/assignment07-mobile-navigation-emerald-01//
+
+
+//--------INSTRUCTIONS--------//
+/*Create navigation appropriate for a small screen, with the menu not visible 
+until the "pancake" bar is clicked.  When clicked, the menu should display, and 
+hide again when clicked.
+Create navigation appropriate for wide screen menu as well.
+Use a "mobile first" approach, with the CSS media query to define styles for wide screens.
+The "hover" state (a:hover) should be visually different*/
+
+//Use JavaScript to respond to the "click" events.                              //
+//Use JavaScript to toggle class values to change the visibility of the menu.   //
+//Suggested Strategy for Approaching this Assignment                //
+//Tackle the small screen version first, in the "expanded" state.   //
+//Then, tackle the "hidden" version of the menu                     //
+//[You could do either the expanded or hidden versions first]       //
+//After the small screen, then tackle the wide screen version.      //
