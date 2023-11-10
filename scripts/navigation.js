30cb5432b44d90c0a0b9296d66a877345e459c29
emerald-01 <teh.sjchem@gmail.com>
2023-11-10 23:50:44 +0800

    updated

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -16,3 +16,18 @@ The "hover" state (a:hover) should be visually different*/
 //Then, tackle the "hidden" version of the menu                     //
 //[You could do either the expanded or hidden versions first]       //
 //After the small screen, then tackle the wide screen version.      //
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM loaded");
+
+    let headerNav = document.querySelector("header nav");
+
+    let headerUl = document.querySelector("header ul");
+
+    headerNav.addEventListener("click", function () {
+        console.log("Nav was clicked");
+
+        headerUl.classList.toggle("collapsed");
+        headerNav.classList.toggle("expanded");
+    })
+})
